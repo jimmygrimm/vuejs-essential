3d67d1818c842d038652b75fe454ac4ef45e169b
Jimmy Grimm <jimmygrimm1943@gmail.com>
2019-11-29 15:58:15 +0800

    articles

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import ls from '../utils/localStorage'
 import router from '../router'
+import * as moreActions from './actions'
 
 Vue.use(Vuex)
 
 const state = {
     user: ls.getItem('user'),
-    auth: ls.getItem('auth')
+    auth: ls.getItem('auth'),
+    articles: ls.getItem('articles')
 }
 
 const mutations = {
@@ -18,32 +20,51 @@ const mutations = {
     UPDATE_AUTH(state, auth) {
         state.auth = auth
         ls.setItem('auth', auth)
+    },
+    UPDATE_ARTICLES(state, articles) {
+        state.articles = articles
+        ls.setItem('articles', articles)
     }
 }
 
 const actions = {
-    login({ commit }, user) {
+    login({commit}, user) {
         if (user) commit('UPDATE_USER', user)
         commit('UPDATE_AUTH', true)
         router.push('/')
     },
-    logout({ commit }) {
+    logout({commit}) {
         commit('UPDATE_AUTH', false)
-        router.push({ name: 'Home', params: { logout: true } })
+        router.push({name: 'Home', params: {logout: true}})
     },
-    updateUser({ state, commit }, user) {
+    updateUser({state, commit}, user) {
         const stateUser = state.user
 
         if (stateUser && typeof stateUser === 'object') {
-            user = { ...stateUser, ...user }
+            user = {...stateUser, ...user}
         }
 
         commit('UPDATE_USER', user)
+    },
+    ...moreActions
+}
+
+const getters = {
+    getArticleById: (state) => (id) => {
+        let articles = state.articles
+
+        if (Array.isArray(articles)) {
+            articles = articles.filter(article => parseInt(id) === parseInt(article.articleId))
+            return articles.length ? articles[0] : null
+        } else {
+            return null
+        }
     }
 }
 
 const store = new Vuex.Store({
     state,
+    getters,
     mutations,
     actions
 })
